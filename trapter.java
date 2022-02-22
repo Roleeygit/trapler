a2935ceba6181a9459aef7173de896274b8002ce
Roleeygit <juhaszroli2001@gmail.com>
2022-02-22 08:51:48 +0100

    oldalak megadva, terület kiszámítva

--- trapter.java
+++ trapter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 * File: trapter.java
 * Author: Juhász Roland
@@ -8,12 +10,29 @@
 * Licenc: GNU GPL
 */
 
-import java.util.Scanner;
-
 class trapter {
 public static void main(String[] args) {
    System.out.println(" Trapéz területe\n Juhász Roland \n 2022-02-22 Szoft I N");
-   Scanner scan = new Scanner(System.in); 
 
+   try (Scanner scan = new Scanner(System.in)) {
+    System.out.print("aoldal:"); 
+       int aoldal = scan.nextInt();  
+    
+       System.out.print("boldal:"); 
+       int boldal = scan.nextInt();
+    
+       System.out.print("coldal:"); 
+       int coldal = scan.nextInt();
+    
+       System.out.print("doldal:"); 
+       int doldal = scan.nextInt();
+    
+       double terület = ((aoldal) + boldal) / 4* (aoldal - coldal)
+       * Math.sqrt((aoldal+boldal-coldal+doldal)*(aoldal-boldal-coldal+doldal)*
+       (aoldal+boldal-coldal-doldal)*(-aoldal+boldal+coldal+doldal));
+       System.out.println("A trapéz területe:" + terület);
 }
 }
+}
+
+
