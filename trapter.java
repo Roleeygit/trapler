31f1a6a66b463d6a13cfa664fb7840c6a21ee461
Roleeygit <juhaszroli2001@gmail.com>
2022-02-22 08:31:10 +0100

    Kezdés

---
+++ trapter.java
@@ -0,0 +1,19 @@
+/*
+* File: trapter.java
+* Author: Juhász Roland
+* Copyright: 2022, Juhász Roland
+* Group: Szoft I N 
+* Date: 2022-02-22
+* Github: https://github.com/Roleeygit/
+* Licenc: GNU GPL
+*/
+
+import java.util.Scanner;
+
+class trapter {
+public static void main(String[] args) {
+   System.out.println(" Trapéz területe\n Juhász Roland \n 2022-02-22 Szoft I N");
+   Scanner scan = new Scanner(System.in); 
+
+}
+}
